abd7023f3b05b94ae7d70a13fa58178734d32398
Cedric Vo <cedricvogt91@gmail.com>
2025-11-02 18:32:58 +0100

    added health route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import express from 'express'; 
+import cors from 'cors'; // allowing API calls from browsers
+
 
 const app = express();
+const PORT = process.env.PORT || 3000; 
 
-console.log(app);
+app.use(express.json());
+app.use(cors());
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'ok',
+        message: 'Liquidity Aggregation API is running'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Health check: http://localhost:${PORT}/health`);
+})
+
